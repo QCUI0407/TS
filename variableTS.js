53c3403ea49eeaa1d6be998c9e6cfff4ae9140ec
cui-homestudy <cuiqiyuan1989@gmail.com>
2023-05-10 23:15:45 -0500

    var string boolean

---
+++ variableTS.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello!!!!!";
+var myNum = 6;
+console.log(greetings);
+//number
+var userId = 123456;
+//boolean
+var isLoggedIn = false;
---
+++ variableTS.ts
@@ -0,0 +1,16 @@
+let greetings: string = "Hello!!!!!";
+
+let myNum = 6
+
+console.log(greetings);
+
+//number
+
+let userId: number = 123456
+
+//boolean
+
+let isLoggedIn: boolean = false
+
+
+export{}
