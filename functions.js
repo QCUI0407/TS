7d290624f53caa53925f98fd9f0459e6a834e4e6
cui-homestudy <cuiqiyuan1989@gmail.com>
2023-05-17 22:53:17 -0500

    function II

--- functions.js
+++ functions.js
@@ -15,3 +15,29 @@ addTwo(10);
 getUpper("abcdefg");
 checkStatus("abc", "abc@mail.com", true);
 loginUser("abc", "abc@MediaList.com");
+//----------- function return
+function addOne(num) {
+    return num + 1;
+}
+// function getValue(myVal: number): boolean{
+//     if(myVal > 5){
+//         return true
+//     }
+//     return "200 ok"
+// }
+var getHello = function (s) {
+    return "";
+};
+/**
+ * map auto check arr's type
+ */
+var heros = ["abc", "def", "ghi"];
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
--- functions.ts
+++ functions.ts
@@ -17,4 +17,37 @@ checkStatus("abc","abc@mail.com",true)
 
 loginUser("abc","abc@MediaList.com")
 
+//----------- function return
+function addOne(num: number): number{
+    return num + 1
+}
+
+// function getValue(myVal: number): boolean{
+//     if(myVal > 5){
+//         return true
+//     }
+//     return "200 ok"
+// }
+
+const getHello = (s:string):string => {
+    return ""
+}
+
+/**
+ * map auto check arr's type
+ */
+const heros = ["abc","def","ghi"]
+
+heros.map(hero =>{
+    return `hero is ${hero}`
+})
+
+function consoleError(errmsg:string): void{
+    console.log(errmsg);   
+}
+
+function handleError(errmsg:string): never{
+   throw new Error(errmsg);  
+}
+
 export{}
