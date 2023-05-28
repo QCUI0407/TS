5ea8ebd823d666ec3838e3efa9da74ce6267e607
cui-homestudy <cuiqiyuan1989@gmail.com>
2023-05-27 22:41:02 -0500

    array

---
+++ arr.js
@@ -0,0 +1,11 @@
+var superHeros = [];
+var allUsers = [];
+var MLModels = [
+    [101, 101, 101, 1],
+    []
+];
+superHeros.push("spiderman");
+allUsers.push({
+    name: "",
+    isActive: true
+});
---
+++ arr.ts
@@ -0,0 +1,20 @@
+const superHeros: string[] = []
+
+type User ={
+    name: string
+    isActive: boolean
+}
+
+const allUsers: User[] = []
+
+const MLModels: number[][] = [
+    [101,101,101,1],
+    []
+]
+
+superHeros.push("spiderman")
+
+allUsers.push({
+    name: "",
+    isActive: true
+})
