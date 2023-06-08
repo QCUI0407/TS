aad36955858ce25bec57df8ad65579efa8fab0a4
cui-homestudy <cuiqiyuan1989@gmail.com>
2023-06-08 00:00:50 -0500

    interface

---
+++ interface.js
@@ -0,0 +1,19 @@
+var qc = {
+    dbId: 1,
+    email: "qc@mail",
+    userId: 123,
+    startTrail: function () {
+        return "acs ascs asdxcx";
+    },
+    stratTrailOne: function () {
+        return "acs ascs asdxcx";
+    },
+    getCoupon: function () {
+        return 10;
+    }
+    // getCoupon: (name: "abcd", off: 10) => {
+    //     return 10
+    // }
+};
+qc.email = "qc1@qc.com";
+//qc.dbId = 2
