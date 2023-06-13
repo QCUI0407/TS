fc0ce5744367f0a686fc5ab80aad9317bab04921
cui-homestudy <cuiqiyuan1989@gmail.com>
2023-06-12 23:51:09 -0500

    interface II

--- interface.ts
+++ interface.ts
@@ -8,10 +8,41 @@ interface User {
     getCoupon(couponname: string, value: number): number
 }
 
+interface User{
+    xxxToken: string
+}
+
+interface Admin extends User{
+    role: "admin" | "ta" | "student"
+}
+
+const c: Admin = {
+    dbId:1,
+    email:"qc@mail",
+    userId: 123,
+    role: "ta",
+    xxxToken: "132546874",
+    startTrail: ()=>{
+        return "acs ascs asdxcx"
+    },
+    stratTrailOne: ()=>{
+        return "acs ascs asdxcx"
+    },
+
+    getCoupon: () => {
+        return 10
+    }
+
+    // getCoupon: (name: "abcd", off: 10) => {
+    //     return 10
+    // }
+}
+
 const qc: User = {
     dbId:1,
     email:"qc@mail",
     userId: 123,
+    xxxToken: "132546874",
     startTrail: ()=>{
         return "acs ascs asdxcx"
     },
